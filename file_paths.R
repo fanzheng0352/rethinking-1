42b4423c4ac2d0b1b7256ebaca17bf20c5a4efdf
Joe Powers <joepowers16@gmail.com>
2019-05-08 13:14:21 -0700

    updates paste_image_path() in file_paths.R

--- file_paths.R
+++ file_paths.R
@@ -1,5 +1,5 @@
 dir_images <- here::here("rethinking_cloud_files", "static", "images")
 
-paste_image_path <- function(image, path_to_images = dir_images){
-  paste(path_to_images, image, sep = "/")
+paste_image_path <- function(image_file, path_to_images = dir_images){
+  fs::path(path_to_images, image_file)
 }
